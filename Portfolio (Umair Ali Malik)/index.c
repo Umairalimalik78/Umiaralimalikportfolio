047f2963b5507f3e79d805457ecc9bdf3011687b
UMAIR ALI MALIK <umairalimalik78@gmail.com>
2023-12-22 13:22:32 +0500

    Add files via upload

---
+++ Portfolio (Umair Ali Malik)/index.c
@@ -0,0 +1,14 @@
+ #include <stdio.h>
+
+int main() {   
+   int a,b ;
+    printf("Enter a number=");
+    scanf("%d",&a);
+    if (a==-a){
+        b=(a*-1);
+        printf("%d",b);  
+    }
+    else 
+        printf("%d",a);
+        return 0;
+}
